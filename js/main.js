2e74e7c0d6b53bcf7fa96f5800a0a47fb293174a
Tyler S <tyler@notnats.com>
2025-10-31 19:30:50 -0400

    Some JS for remembering the state of the sidebar.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,17 @@ const sidebar = document.getElementById("sidebar");
 const menuBtn = document.getElementById("menu-button");
 const icon = menuBtn.querySelector('i');
 
+if (localStorage.getItem("sidebar-collapsed") == "true") {
+    sidebar.classList.add("collapsed");
+}
+
 menuBtn.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
 
+    // Sets if sidebar is collapsed. Animation still plays. Needs refinement.
+    const isCollapsed = sidebar.classList.contains("collapsed");
+    localStorage.setItem("sidebar-collapsed", isCollapsed);
+
     lucide.createIcons();
 });
+
